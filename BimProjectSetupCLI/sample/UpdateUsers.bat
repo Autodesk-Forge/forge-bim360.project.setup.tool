2380a2a09dbbd1fcd487328b68f35988c5494e5a
Zhong <wuzhong1980@gmail.com>
2020-10-13 08:15:14 +0800

    update the docs to support user update

--- BimProjectSetupCLI/sample/UpdateUsers.bat
+++ BimProjectSetupCLI/sample/UpdateUsers.bat
@@ -2,5 +2,5 @@
 set FORGE_CLIENT_SECRET=<your_client_secret_without_quotation_marks>
 set FORGE_BIM_ACCOUNT_ID=<your_account_id_without_quotation_marks>
 cd ..
-Autodesk.BimProjectSetup.exe -u ".\sample\BIM360_ProjectUser_Template.csv" -h "admin@company.com" --UP
+Autodesk.BimProjectSetup.exe -u ".\sample\BIM360_ProjectUser_UP_Template.csv" -h "admin@company.com" --UP
 pause
